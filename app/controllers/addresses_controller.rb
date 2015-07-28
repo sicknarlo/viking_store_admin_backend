ce12cf6107a71c7aff9bcd23074c5b1bc99e3efe
Alice Fung <alice.o.fung@gmail.com>
2015-07-28 19:58:50 -0400

    user filter address index

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,15 +1,17 @@
 class AddressesController < ApplicationController
 
 def new
-
+  @addresses = Address.new( :user_id => params[:user_id])
 end
 
 def index
-  # if User.exists?( params[:user_id])
-  	@address ||= Address.where("user_id = params[:id]") 
-		@address ||= Address.all
-	# 
-	# @addresses = User.where( id: params[:user_id] ).first.addresses
+  if User.exists?( params[:user_id])
+    @addresses = Address.where( :user_id => params[:user_id])
+    @user = User.find(params[:user_id])
+    # @addresses ||= Address.where("user_id = params[:id]")
+  else
+    @addresses = Address.all
+  end
 end
 
 
