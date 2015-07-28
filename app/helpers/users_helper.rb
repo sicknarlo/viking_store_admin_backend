0cebdf3c38044fe93d472576040e8dbd21c22abd
Alice Fung <alice.o.fung@gmail.com>
2015-07-28 17:15:19 -0400

    moved placeholder method to userhelper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+  def display_address(type, user)
+    if user.default_billing_address && type == "billing"
+      user.default_billing_address.full_address
+    elsif user.default_shipping_address && type == "shipping"
+      user.default_shipping_address.full_address
+    else
+      "No default #{type} address on file."
+    end
+  end
 end
